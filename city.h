ba7250c147a58e1f62de5f852d401982c388650a
SW <sw@gmail.com>
2026-05-18 19:04:10 +0600

    City Scene: add city scene files (buildings, road, windows)

---
+++ city.h
@@ -0,0 +1,13 @@
+#ifndef CITY_H
+
+#define CITY_H
+
+// ================= DRAW =================
+
+void drawCityScene(float alpha);
+
+// ================= UPDATE =================
+
+void updateCityAnimation();
+
+#endif
